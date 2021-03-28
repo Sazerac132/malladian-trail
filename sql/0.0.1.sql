bb97ee63b36f1755d0f44eb8eb31c3bda3660556
Tommy <rejectmusician@hotmail.co.uk>
2021-03-28 18:54:48 +0100

    - Added SQL table for games. - Added function for adding game with unique game_code.

--- sql/0.0.1.sql
+++ sql/0.0.1.sql
@@ -5,3 +5,30 @@ CREATE TABLE tversion (
 );
 
 INSERT INTO tversion VALUES ('0.0.1');
+
+CREATE TABLE tgame (
+  id INT NOT NULL AUTO_INCREMENT,
+  game_code INT NOT NULL UNIQUE,
+  PRIMARY KEY (id)
+);
+
+DELIMITER //
+
+DROP FUNCTION IF EXISTS get_unique_game_code//
+
+CREATE FUNCTION get_unique_game_code()
+RETURNS INT
+READS SQL DATA
+
+BEGIN
+
+	SET @game_code = (SELECT FLOOR(RAND() * 900000 + 100000));
+
+	WHILE EXISTS (SELECT game_code FROM tgame WHERE game_code = @game_code) DO
+		SET @game_code = (SELECT FLOOR(RAND() * 900000 + 100000));
+	END WHILE;
+	
+	RETURN @game_code;
+END; //
+
+DELIMITER ;
