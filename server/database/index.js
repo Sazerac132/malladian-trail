ef8cd290ad9951cb289b3be988fab315f0a1bc95
Tommy <rejectmusician@hotmail.co.uk>
2021-03-30 19:06:27 +0100

    Added params capability to db driver.

--- server/database/index.js
+++ server/database/index.js
@@ -11,11 +11,12 @@ class Database {
     };
 
     this.connection = mysql.createConnection(this.config);
+    this.connection.config.namedPlaceholders = true;
   }
 
-  query(sql) {
+  query(sql, parameters = {}) {
     return new Promise((resolve, reject) => {
-      this.connection.query(sql, (err, results, fields) => {
+      this.connection.execute(sql, parameters, (err, results, fields) => {
         if (err) {
           reject(err);
           return;
